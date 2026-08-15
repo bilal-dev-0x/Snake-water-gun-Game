5c8325faa0e1340ae852d70607c1b5465d55973c
Bilal Aslam <bilalaslam130607@gmail.com>
2026-08-15 11:22:49 +0500

    Simplify snake water gun game logic

--- Snake-water-gun.py
+++ Snake-water-gun.py
@@ -1,55 +1,36 @@
 import random
 
-'''
-1 for snake
--1 for water
-0 for gun
-'''
+choices = {
+    "s": "snake",
+    "w": "water",
+    "g": "gun",
+}
 
-computer = random.choice([1, 0, -1])
-youstr = input("Enter your choice : ")
-youdict = {"s" : 1, "w" : -1, "g" : 0}
-reversedict = {1 :"snake", 0 : "gun", -1 :"water"}
-you = youdict[youstr]
+winning_pairs = {
+    ("snake", "water"),
+    ("water", "gun"),
+    ("gun", "snake"),
+}
 
-print(f"Computer chose: {reversedict[computer]}")
-print(f"You chose: {reversedict[you]}")
+print("Snake Water Gun Game")
+print("Choose one: s for snake, w for water, g for gun")
 
-if computer == -1 and you == 1:
-    print("Snake drinks water 🐍💧")
-    print("You win....!")
+user_key = input("Enter your choice: ").strip().lower()
 
-elif computer == -1 and you == 0:
-    print("Water drown gun💧🔫")
-    print("You lose....!")
-
-elif computer == -1 and you == -1:
-    print("Both choose water💧")
-    print("match draw....!")
-
-elif computer == 1 and you == 1:
-    print("Both chose Snake 🐍")
-    print("match draw....!")
-
-elif computer == 1 and you == 0:
-    print("Gun shoots snake 🔫🐍")
-    print("You win....!")
-
-elif computer == 1 and you == -1:
-    print("Snake drinks water 🐍💧")
-    print("You lose....!")
-
-elif computer == 0 and you == 1:
-    print("Gun shoots snake 🔫🐍")
-    print("You lose....!")
-
-elif computer == 0 and you == -1:
-    print("Water drowns gun 💧🔫")
-    print("You win....!")
-
-elif computer == 0 and you == 0:
-    print("Both chose Gun 🔫")
-    print("match draw....!")
-
+if user_key not in choices:
+    print("Invalid choice. Please run the game again and choose s, w, or g.")
 else:
-    print("Something wrong......!")
+    user_choice = choices[user_key]
+    computer_choice = random.choice(list(choices.values()))
+
+    print(f"Computer chose: {computer_choice}")
+    print(f"You chose: {user_choice}")
+
+    if user_choice == computer_choice:
+        print("Match draw!")
+    elif (user_choice, computer_choice) in winning_pairs:
+        print(f"{user_choice.title()} beats {computer_choice}.")
+        print("You win!")
+    else:
+        print(f"{computer_choice.title()} beats {user_choice}.")
+        print("You lose!")
